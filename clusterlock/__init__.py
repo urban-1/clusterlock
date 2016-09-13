69b7252d9f9ae39301cf6f2832536bd5006c6c74
Andreas Bontozoglou <andreas.bontozoglou@sky.uk>
2016-09-13 17:54:31 +0100

    [Events] Another table to detect stale sessions/locks

--- clusterlock/__init__.py
+++ clusterlock/__init__.py
@@ -3,7 +3,7 @@ import traceback
 import threading
 import time
 
-from sqlalchemy import Column, Integer, String, Boolean, UniqueConstraint, Sequence, Text
+from sqlalchemy import Column, Integer, String, Boolean, UniqueConstraint, Sequence, Text, ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship, scoped_session, sessionmaker, class_mapper
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -35,15 +35,30 @@ class CustomBase(object):
 Base = declarative_base(cls=CustomBase)
 
 
-class LockLine(Base):
-    __tablename__ = 'cluster_lock_line'
-    __table_args__ = (UniqueConstraint('what', 'context', name='_LockLine_uniq'),)
+class LockContext(Base):
+    __tablename__ = 'cluster_lock_ctx'
+    __table_args__ = (UniqueConstraint('what', 'context', name='_LockContext_uniq'),)
     
-    id = Column(Integer, Sequence('seq_cluster_lock_id'), primary_key=True, autoincrement=True)
+    id = Column(Integer, Sequence('seq_cluster_lock_ctx_id'), primary_key=True, autoincrement=True)
     what = Column(String(512))       # What do we lock
     count = Column(Integer)          # Count for semaphores
-    who = Column(String(512))        # Who locked it last
-    context = Column(String(512))  # Which software context (application)
+    context = Column(String(512))    # Which software context (application)
+    events = relationship("LockEvent", cascade="all, delete-orphan")
+
+    
+
+class LockEvent(Base):
+    __tablename__ = 'cluster_lock_event'
+    __table_args__ = (
+        PrimaryKeyConstraint('context_id', 'started', 'who'),
+    )
+    
+    #id = Column(Integer, Sequence('seq_cluster_lock_evt_id'), primary_key=True, autoincrement=True)
+    context_id = Column(Integer, ForeignKey('cluster_lock_ctx.id'))
+    started = Column(Integer)                  # When this started, epoch
+    ended = Column(Integer, default=0)         # When this ended, epoch
+    who = Column(String(512))                  # Who locked it last
+    
     
 
 class ClusterLockBase(object):
@@ -60,17 +75,23 @@ class ClusterLockBase(object):
         self._max_bound = max_bound
         self._time_slept = 0
         self._sleep_interval = sleep_interval
+        self._events = []
         
         # Create schema
-        Base.metadata.create_all(self._engine)
+        try:
+            Base.metadata.create_all(self._engine)
+        except:
+            pass
+        
         Base.query = self._session.query_property()
         
-        # Create a LockLine for this context
-        ll = LockLine(
-            what=what,
-            count=value,
-            context = context)
         try:
+            # Create a LockContext for this context
+            ll = LockContext(
+                what=what,
+                count=value,
+                context = context)
+            
             self._session.add(ll)
             self._session.commit()
         except:
@@ -95,10 +116,9 @@ class ClusterLockBase(object):
         while rc == 0:
             lg.debug("Trying")
             try:
-                rc = LockLine.query.filter(LockLine.what==self._what)\
-                                .filter(LockLine.context==self._context)\
-                                .filter(LockLine.count > self._min_bound).update({"count": LockLine.count - 1,
-                                                                                  "who": who})
+                rc = LockContext.query.filter(LockContext.what==self._what)\
+                                .filter(LockContext.context==self._context)\
+                                .filter(LockContext.count > self._min_bound).update({"count": LockContext.count - 1})
                 
                 lg.debug("Acquire lock resulted in %d rows affected" % rc)
                 self._session.commit()
@@ -113,6 +133,19 @@ class ClusterLockBase(object):
                 
                 time.sleep(0.1)
                 self._time_slept += self._sleep_interval
+            else:
+                # Create our event...
+                ctx = LockContext.query.filter(LockContext.what==self._what and \
+                                               LockContext.context==self._context).one()
+                self._events.append(LockEvent(
+                        context_id=ctx.id,
+                        started=int(time.time()),
+                        who = who
+                        ))
+                
+                self._session.add(self._events[-1])
+                self._session.commit()
+                
         
     def release(self):
         """
@@ -121,33 +154,28 @@ class ClusterLockBase(object):
         """
         who = get_who()
         rc = 0
-        while rc == 0:
-            try:
-                
-                ## 2nd way
-                rc = LockLine.query.filter(LockLine.what==self._what)\
-                                   .filter(LockLine.context==self._context)\
-                                   .filter(LockLine.count < self._max_bound).update({"count": LockLine.count + 1, 
-                                                                                    "who": who})
-                
-                lg.debug("Release lock resulted in %d rows affected" % rc)
-                self._session.commit()
-            except:
-                self._session.rollback()
-                lg.debug(traceback.format_exc())
-    
-    def reset(self, value=None):
-        """
-        Reset to a specific value
-        """
-        if value is None:
-            value = self._max_bound
+        
+        try:
             
-        LockLine.query.filter(LockLine.what==self._what)\
-                      .filter(LockLine.context==self._context)\
-                      .filter(LockLine.count < self._max_bound).update({"count": value, "who": ""})
-        lg.debug("Release lock resulted in %d rows affected" % rc)
-        self._session.commit()
+            ## 2nd way
+            rc = LockContext.query.filter(LockContext.what==self._what)\
+                                .filter(LockContext.context==self._context)\
+                                .filter(LockContext.count < self._max_bound).update({"count": LockContext.count + 1})
+            
+            lg.debug("Release lock resulted in %d rows affected" % rc)
+            self._session.commit()
+            
+            # Delete our event...
+            if len(self._events):
+                self._session.delete(self._events[-1])
+                self._session.commit()
+        except:
+            self._session.rollback()
+            print(traceback.format_exc())
+            lg.debug(traceback.format_exc())
+            
+        if rc != 1:
+            raise ClusterLockReleaseError("Number of releases mismatch! Someone consumes more!")
 
 
 class Lock(ClusterLockBase):
--- tests/spawn.sh
+++ tests/spawn.sh
@@ -11,7 +11,7 @@ function ctrl_c() {
     exit 0
 }
 
-for i in {1..24}
+for i in {1..2}
 do
     echo "Spawning $i"
     $app "job-$i" &
